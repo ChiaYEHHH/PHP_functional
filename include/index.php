372750f0147f28b414cf34b5fd741bebd04eca39
ChiaYEHHH <hedy821216@gmail.com>
2024-05-07 16:42:23 +0800

    完成include練習

--- include/index.php
+++ include/index.php
@@ -13,6 +13,7 @@
             height: 100vh;
             position: relative;
         }
+
         header {
             height: 150px;
             width: 100%;
@@ -22,22 +23,27 @@
             text-align: center;
             line-height: 150px;
         }
+
         nav {
             margin: 5px auto;
         }
+
         nav a {
             display: inline-block;
             padding: 5px 12px;
             text-align: center;
             font-size: 14px;
         }
+
         nav a:hover {
             text-decoration: underline;
         }
+
         main {
             padding: 20px;
             font-size: 32px;
         }
+
         footer {
             height: 35px;
             width: 80%;
@@ -47,14 +53,58 @@
             position: fixed;
             bottom: 0;
         }
+
+        .active {
+            background-color: skyblue;
+        }
     </style>
 </head>
 
 <body>
-    <?php include_once "./layouts/header.php"; ?>
-    <?php include_once "./layouts/nav.php"; ?>
+    <!-- basename(__FILE__)取得當下檔名XXX.php,再用explode以"."為界線炸開組成陣列['XXX' , 'php'] 
+    只取[0]就是['XXX']-->
+    <?php // $file = explode(".", basename(__FILE__))[0]; 
+    ?>
+    <!-- 判斷PHP檔名是否存在，存在$page='當下頁面'，不存在$page='main' -->
+    <?php $page = isset($_GET['page']) ? $_GET['page'] : 'main'; ?>
+    <!-- include跟 include_once差別在include_once同樣的檔案只會執行一次 -->
+    <?php include_once "./layout/header.php"; ?>
+    <?php include_once "./layout/nav.php"; ?>
     <marquee>這是一個跑馬燈</marquee>
-    <main>主要內容</main>
+
+
+
+    <?php
+    // 精簡版
+    // $file
+    $file = "./pages/{$page}.php";
+    // 用file_exist()判斷檔案是否存在
+    if (file_exists($file)) {
+
+        include $file;
+    } else {
+        include "./pages/main.php";
+    }
+
+
+    // 第一版
+    // switch ($page){
+    //     case 'index':
+    //         include "./pages/main.php";
+    //         break;
+    //     case 'news':
+    //         include "./pages/news.php";
+    //         break;
+    //     case 'products':
+    //         include "./pages/products.php";
+    //         break;
+    //     case 'contact':
+    //         include "./pages/contact.php";
+    //         break;
+    //     default :
+    //     include "./pages/main.php";
+    // } 
+    ?>
     <footer>頁腳</footer>
 </body>
 
